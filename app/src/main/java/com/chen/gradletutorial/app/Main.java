efa3a70115d0177e8cca756462e3146cb238f5fb
chen <39870481+cpupg@users.noreply.github.com>
2024-05-10 23:48:32 +0800

    增加依赖

--- app/src/main/java/com/chen/gradletutorial/app/Main.java
+++ app/src/main/java/com/chen/gradletutorial/app/Main.java
@@ -1,13 +1,26 @@
 package com.chen.gradletutorial.app;
 
 import com.chen.gradletutorial.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.Properties;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         UserService userService = new UserService();
         String name = userService.getName();
         // 中文乱码，暂时无法解决
         // System.out.println("用户名:" + name);
         System.out.println("username:" +name);
+        Properties properties = System.getProperties();
+        ObjectMapper objectMapper = new ObjectMapper();
+        if (!new File("temp").exists()) {
+            new File("temp").mkdir();
+        }
+        OutputStream os = new FileOutputStream("temp/system.json");
+        objectMapper.writerWithDefaultPrettyPrinter().writeValue(os, properties);
     }
 }
---
+++ app/src/test/java/com/chen/gradletutorial/app/AppTest.java
@@ -0,0 +1,10 @@
+package com.chen.gradletutorial.app;
+
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void t() {
+        System.out.println("hello,junit");
+    }
+}
